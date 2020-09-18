6aef6c27fb6d5fda53fcfb12ed2f88f4d378401f
willg <wlhowell6238@eagle.fgcu.edu>
2020-09-18 15:35:59 -0400

    added content to read me file and fixed up some comments.

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # OOP_Semester_Project_WH
-This project is an assigned semester long project. It will be used to show
-that I can use databases and GUI together.
+##Name: William Howell
+######last update: 9/18/2020
+
+
+This project is an assigned semester long project. 
+It will be used to show that I can use databases and GUI together.
+As of currently the GUI can add data to the database through a 
+prepared sql statement. I have also populated a combo box with numbers
+to allow users to choose a quantity of a certain product.
+
+
+######resources used include: 
+1. classmates
+1. Vladimir Hardy
+1. http://tutorials.jenkov.com/jdbc/preparedstatement.html
--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,6 +1,7 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+
 import java.sql.PreparedStatement;
 
 import java.sql.*;
@@ -68,9 +69,7 @@ public class Controller {
             //STEP 3: Execute a query
             stmt = conn.createStatement();
 
-            /**
-             * prepared statement that inserts data into the database.
-             */
+            //prepared statement that inserts data into the database.
             String insertSql = "INSERT INTO Product(type, manufacturer, name) VALUES ( ? , ? , ? )";
             PreparedStatement ist = conn.prepareStatement(insertSql);
             ist.setString(1, itemType.getValue());
@@ -79,9 +78,8 @@ public class Controller {
 
             ist.executeUpdate();
 
-            /**
-             * prints to console all the products in the database after button click.
-             */
+
+            //prints to console all the products in the database after button click.
             String sql = "select id, name, type, manufacturer from PRODUCT";
 
             ResultSet rs = stmt.executeQuery(sql);
@@ -96,10 +94,7 @@ public class Controller {
             // STEP 4: Clean-up environment
             stmt.close();
             conn.close();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-
-        } catch (SQLException e) {
+        } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
     }
